9a2e590e1ee46cc63b1c0f1d19016fd56abd535a
Korosium <korosium21@gmail.com>
2025-02-16 16:01:39 -0500

    Added the error messages to the register page

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -33,9 +33,12 @@ def home():
 def about():
     return render_template('about.html', title="About")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form)
 
 @app.route("/login")
