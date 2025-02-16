ab3a42a6e5156e5b7b2747e98cdc26cc8a9fb339
Korosium <korosium21@gmail.com>
2025-02-16 15:43:59 -0500

    Added the register page logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -31,6 +32,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
