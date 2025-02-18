b6836c4c6687c617125c8777018044ae68201390
Korosium <korosium21@gmail.com>
2025-02-18 18:32:44 -0500

    Reset HTML templates and routes created

--- enigma/routes.py
+++ enigma/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from enigma import app, db, argon2
-from enigma.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from enigma.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from enigma.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -137,3 +137,23 @@ def user_posts(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('user_posts.html', posts=posts, user=user)
+
+@app.route("/reset_password", methods=["GET", "POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+    return render_template('reset_request.html', title="Reset Password", form=form)
+
+@app.route("/reset_password/<token>", methods=["GET", "POST"])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is an invalid or expired token.", "warning")
+        return redirect(url_for("reset_request"))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title="Reset Password", form=form)
